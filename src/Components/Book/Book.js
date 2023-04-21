13e32c610fc43395ffcd824bdad07f8e30acdb0b
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-21 11:05:53 -0600

    feat: display sample data books on page load

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,10 +1,11 @@
 import './Book.css'
 
-const Book = ({ img, title, id }) => {
+const Book = ({ img, title, author, id }) => {
   return (
     <div className='book-card'>
       <img src={img}></img>
       <h2>{title}</h2>
+      <h3>By {author}</h3>
     </div>
   )
 }
--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Book from '../Book/Book';
+import './Books.css'
 import { useState, useEffect } from 'react';
 
 const Books = ({ allBooks }) => {
   console.log(allBooks.results.books)
   const bookCards = allBooks.results.books.map(book => {
     return (
-      <h2>hello</h2>
+      <Book
+        img={book.book_image}
+        title={book.title}
+        author={book.author}
+      />
     )
   })
 
