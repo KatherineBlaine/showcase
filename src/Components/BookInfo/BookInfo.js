b7f5cb9b6b0e92fd1190bdf717459c698a435c0d
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-21 11:53:12 -0600

    feat: build book info page layout with sample data

---
+++ src/Components/BookInfo/BookInfo.js
@@ -0,0 +1,20 @@
+import sampleBook from '../../sampleData/sampleBook'
+import './BookInfo.css'
+
+const BookInfo = ({ selectedBook }) => {
+  console.log(selectedBook)
+  return (
+    <div className='book-info'>
+      <img src={selectedBook.book_image}></img>
+        <div className='right-content'>
+          <h1 className='book-title'>{selectedBook.title}</h1>  
+          <h2 className='description'>{selectedBook.description}</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>#{selectedBook.rank} on NYT bestsellers list</p>
+        <p>{selectedBook.weeks_on_list} weeks on NYT bestseller list</p>
+        </div>
+    </div>
+  )
+}
+
+export default BookInfo;
