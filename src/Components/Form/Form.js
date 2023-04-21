5db3c96e78e6fac6d9ecc031edd1802774816bbd
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-21 13:49:52 -0600

    feat: add controlled form to track search query string value

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -2,8 +2,15 @@ import { useState, useEffect } from "react";
 import './Form.css'
 
 const Form = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  // const [filteredBooks, setFilteredBooks] = useState([])
+
+console.log(searchQuery)
   return (
-    <input type="search"></input>
+    <form>
+      <input type="search" onChange={(event) => setSearchQuery(event.target.value)}></input>
+      <button>Search</button>
+    </form>
   )
 }
 
