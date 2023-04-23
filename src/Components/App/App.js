6626f1ab3bba505aa1f6b352b0b4e970859e623f
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-23 16:57:00 -0600

    refac: clean up App return statement

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -31,8 +31,26 @@ const App = () => {
     getBooks()
   }, [])
 
+  const showSearchResults = (match) => {
+    const filteredBooks = allBooks.filter(book => book.title.includes(match.params.searchQuery) || book.title.includes(match.params.searchQuery.toUpperCase()))
+    return (
+      <div>
+        <Form />
+        <Books booksToDisplay={filteredBooks}/>
+      </div>
+    )}
+
+  const showBookInfo = (match) => {
+    const selectedBook = allBooks.find(book => book.primary_isbn10 === match.params.id)
+    return (
+      <BookInfo selectedBook={selectedBook}/>
+    )
+  }
+
   const loadingPage = loading && <h3 className='loading'>LOADING</h3>
+
   const errorPage = error !== '' && <h3 className='error'>Error: {error}, please try again.</h3>
+  
   const mainPage = !loading && error === '' ? <div><Form /><Books booksToDisplay={allBooks}/></div> : null
 
   return (
@@ -44,21 +62,8 @@ const App = () => {
           {errorPage}
           {mainPage}
         </Route>
-        <Route path='/search/:searchQuery' render={({match}) => {
-          const filteredBooks = allBooks.filter(book => book.title.includes(match.params.searchQuery) || book.title.includes(match.params.searchQuery.toUpperCase()))
-          return (
-            <div>
-              <Form />
-              <Books booksToDisplay={filteredBooks}/>
-            </div>
-          )}}>
-        </Route>
-        <Route path='/book/:id' render={({match}) => {
-          const selectedBook = allBooks.find(book => book.primary_isbn10 === match.params.id)
-          return (
-            <BookInfo selectedBook={selectedBook}/>
-          )}}>
-        </Route>
+        <Route path='/search/:searchQuery' render={({match}) => showSearchResults(match)}></Route>
+        <Route path='/book/:id' render={({match}) => showBookInfo(match)}></Route>
         <Route path='*'>
           <NotFound/>
         </Route>
