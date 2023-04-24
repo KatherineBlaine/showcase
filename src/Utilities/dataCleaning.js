3d04022d6a269625d9872633a0a9565b05dba649
Katherine Blaine <blaine.katherine12@gmail.com>
2023-04-23 18:05:44 -0600

    refac: break up cleanData function into multiple single responsibility functions and remove logic from api call

--- src/Utilities/dataCleaning.js
+++ src/Utilities/dataCleaning.js
@@ -1,17 +1,34 @@
-const cleanData = (bookData) => {
-  bookData.forEach(book => {
+const pullBooksFromOverview = (data) => {
+  return data.results.lists.reduce((accumulator, currentlist) => {
+    currentlist.books.forEach(book => {
+      book.genre = currentlist.list_name;
+      accumulator.push(book);
+    })
+    return accumulator;
+  }, [])
+}
+
+const assignISBN = (data) => {
+  data.forEach(book => {
     if(book.primary_isbn10 === '' || 'none') {
       book.primary_isbn10 = book.primary_isbn13;
     }
   })
+}
 
-  const uniqueBooks = bookData.reduce((accumulator, currentBook) => {
+const removeRepeats = (data) => {
+  return data.reduce((accumulator, currentBook) => {
     if (!accumulator.find((book) => book.title === currentBook.title)) {
       accumulator.push(currentBook);
     }
     return accumulator;
   }, []);
-  return uniqueBooks;
+}
+
+const cleanData = (data) => {
+  const books = pullBooksFromOverview(data)
+  assignISBN(books)
+  return removeRepeats(books)
 }
 
 export default cleanData;
